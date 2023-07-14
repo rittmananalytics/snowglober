c88c66878a04c54cc3d574ab968ebb483c9c1eab
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-07-14 16:06:06 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='snowglober',
-    version='0.2.0',
+    version='0.3.0',
     author='Amir Jaber',
     author_email='amir@rittmananalytics.com',
     install_requires=[
