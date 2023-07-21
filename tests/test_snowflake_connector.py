6e50162261fc95abada8520d51cca65a65dad111
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-07-21 12:01:31 +0200

    update for clarity

--- tests/test_snowflake_connector.py
+++ tests/test_snowflake_connector.py
@@ -9,9 +9,14 @@ class TestSnowflakeConnector(unittest.TestCase):
         self.connector = SnowflakeConnector()
     
     def test_get_all_snowflake_objects_of_a_resource_types(self):
-        entities = 'schemas'
-        resource_objects = self.connector.get_all_objects_of_a_resource_type(entities)
-        print(f"!!!!!!!!!!!!! Starting test_get_all_{entities} !!!!!!!!!!!!! \n")
+        """Test that we can get all objects of a resource type from Snowflake.
+        to run: 
+            change entities_type
+            run: python -m unittest tests.test_snowflake_connector.TestSnowflakeConnector.test_get_all_snowflake_objects_of_a_resource_types
+        """
+        entities_type = 'schemas'
+        resource_objects = self.connector.get_all_objects_of_a_resource_type(entities_type)
+        print(f"!!!!!!!!!!!!! Starting test_get_all_{entities_type} !!!!!!!!!!!!! \n")
         for resource_object in resource_objects:
             pprint(resource_object)
             print()  # Add an empty line for better separation between items
